ce1664b5b5fe292ff11f09e09955e90aa5afc81a
Nicolai Robles <nicolai.robles@gmail.com>
2016-01-20 17:06:33 -0500

    initial commit

---
+++ server_plan.rb
@@ -0,0 +1,21 @@
+HTTP Request:
+	get  
+	post
+	delete
+	update
+
+HTTP Routes:
+	Login/Signup
+		"/"
+		"/login"
+		"/login_success"
+		"/signup"
+		"/signup_success"
+	Categories Page
+		"/categories"
+		"/categories/:id"
+	Articles Page
+		"/articles"
+		"articles/:id"
+		"articles/:id/edit"
+
