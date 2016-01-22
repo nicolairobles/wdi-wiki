9a7f37fb7fb32c607e905de924d08b1776b81163
Nicolai Robles <nicolai.robles@gmail.com>
2016-01-22 18:28:50 -0500

    set sign-up functionality

--- server.rb
+++ server.rb
@@ -3,11 +3,21 @@ require "pg"
 require "bcrypt"
 require "pry"
 require "date"
+require "time"
 
 module WDIWiki
 	
 	class Server < Sinatra::Base
 		set :method_override, true
+		enable :sessions
+
+		def current_user
+			if session["user_id"]
+				@user ||= db.exec_params(<<-SQL, [session["user_id"]]).first
+				SELECT * FROM users WHERE id = $1
+				SQL
+			end
+		end
 
 		get "/" do
 			redirect "/signup"
@@ -15,7 +25,48 @@ module WDIWiki
 
 		get "/signup" do
 			erb :signup
-		end	
+		end
+
+		post "/signup" do
+			db = database_connection
+			name = params[:name]
+			email = params[:login_email]
+			encrypted_password = BCrypt::Password.create(params[:login_password])
+			users = db.exec_params("INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id", [name, email, encrypted_password])
+	    session["user_id"] = users.first["id"]
+	    binding.pry
+			erb :login 
+		end
+
+		get "/login" do
+			erb :login
+		end
+
+		post "/login" do
+			@user = @@db.exec_params("SELECT * FROM users WHERE login_name = $1", [params[login_name]])
+			# if @user.length == 0
+			# 	@error = "Invalid Username"
+			# 	erb :login
+			# else
+			if @user && params[:login_password] == BCrypt::Password.new(@user["login_password_digest"])
+					session["user_id"] = @user["id"]
+			end
+
+			login_password = BCrypt::Password.new(@user) # the password already in db
+			login_name = params[:login_name]
+	 		users = @@db.exec_params(<<-SQL, [params[:login_name],login_password]) 
+	      SELECT * FROM users WHERE login_name = ;
+	    SQL
+	    if login_password == users["login_password_digest"]
+		    session["user_id"] = users.first["id"]
+	    	binding.pry
+				erb :login_success
+			else
+	  	end
+		end
+
+
+
 
 		get "/articles/:id" do
 			db = database_connection
@@ -27,7 +78,7 @@ module WDIWiki
 				ON users.id = articles.author_ID 
 				WHERE articles.id = #{@id}"
 				).first
-			@time = Date.parse(@article["edit_date"])
+			@time = DateTime.parse(@article["edit_date"])
 			binding.pry
 			erb :article
 		end
@@ -58,6 +109,24 @@ module WDIWiki
 				WHERE articles.id = #{@id}"
 				).first
 			erb :article_edit
+		end
+
+		get "/categories" do
+			db = database_connection
+			@categories = db.exec(
+				"SELECT id, title FROM categories"
+				).to_a
+			erb :categories
+		end
+
+		get "/category/:id" do
+			db = database_connection
+			@id = params[:id]
+			@title = db.exec("SELECT title FROM categories WHERE id = #{@id}").first["title"]
+			@articles = db.exec(
+				"SELECT articles.id AS article_id, articles.title AS article_title, articles.content AS article_content FROM articles INNER JOIN articles_categories ON articles.id = articles_categories.articles_id WHERE articles_categories.categories_id = #{@id}"
+				).to_a
+			erb :category
 		end
 
 		private
